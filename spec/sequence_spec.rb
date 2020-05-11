f44f73d7e409f2644943416b3d950ce1f6937330
Timur Vafin <timur.vafin@flatstack.com>
2020-05-12 00:11:15 +0300

    Upgrade Ruby, RSpec (#5)
    * Update ruby to 2.7.1
    
    * Upgrade RSpec
    
    * Correct spelling

--- spec/sequence_spec.rb
+++ spec/sequence_spec.rb
@@ -1,30 +1,28 @@
-require "spec_helper"
-
 require "sequence"
 
 describe Sequence do
   let(:sequence) { Sequence.new("1") }
 
-  describe "to_s" do
-    it "return initial value" do
+  describe "#to_s" do
+    it "returns initial value" do
       expect(sequence.to_s).to eql "1"
     end
   end
 
-  describe "next" do
-    it "generate next state" do
+  describe "#next" do
+    it "generates next state" do
       expect(sequence.next.to_s).to eql "11"
     end
 
-    it "return Sequence instance" do
+    it "returns Sequence instance" do
       expect(sequence.next).to be_an_instance_of(Sequence)
     end
 
-    it "generate next state 2 times" do
+    it "generates next state 2 times" do
       expect(sequence.next.next.to_s).to eql "21"
     end
 
-    it "generate next state 3 times" do
+    it "generates next state 3 times" do
       expect(sequence.next.next.next.to_s).to eql "1211"
     end
   end
